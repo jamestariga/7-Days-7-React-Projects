e067f19084908589c049d0d5b1de789a353aafd6
James <jamestariga08@live.com>
2022-04-29 11:05:42 -0700

    ✨ Minor Update and README.md Updated

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,26 +2,22 @@ import { useState, useEffect } from 'react'
 import CreatePost from '../Post/CreatePost'
 import PostList from '../Post/PostList'
 
-const getLocalStorage = () => {
-  let localPosts = localStorage.getItem('posts')
-  if (localPosts) {
-    return (localPosts = JSON.parse(localPosts))
-  } else {
-    return []
-  }
-}
-
 const Main = () => {
+  const getLocalStorage = () => {
+    let localPosts = localStorage.getItem('posts')
+    if (localPosts !== null) {
+      return (localPosts = JSON.parse(localPosts))
+    } else {
+      return []
+    }
+  }
+
   const [name, setName] = useState('')
   const [post, setPost] = useState('')
   const [posts, setPosts] = useState(getLocalStorage())
 
   useEffect(() => {
-    const saveLocalStorage = () => {
-      localStorage.setItem('posts', JSON.stringify(posts))
-    }
-
-    saveLocalStorage()
+    localStorage.setItem('posts', JSON.stringify(posts))
   }, [posts])
 
   return (
